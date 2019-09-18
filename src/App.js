ae0ea769dbd0bf83a8059c1302130bbe15edf90c
MerylGeugies <mgeugies@gmail.com>
2019-09-18 15:16:56 +0200

    Set up dashboard with notifications

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 
 class App extends Component {
