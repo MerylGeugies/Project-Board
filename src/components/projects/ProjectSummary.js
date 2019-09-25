b7f2c3845e96f487120e3ed8a6890bb50171f669
MerylGeugies <mgeugies@gmail.com>
2019-09-25 17:07:56 +0200

    Implemented moment to have a proper date and time on the projects

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,8 +5,8 @@ const ProjectSummary = ({project}) => {
     <div className='card z-depth-0 project-summary'>
       <div className='card-content grey-text text-darken-3'>
         <span className='card-title'>{project.title}</span>
-        <p> Posted by Meryl</p>
-        <p className='grey-text'> 02 februari</p>
+        <p> Posted by { project.authorFirstName} {project.authorLastName}</p>
+        <p className='grey-text'> { project.createdAt}</p>
       </div>
     </div>
   )
